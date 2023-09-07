cacbb54b513682c18eab6d25adfde0e8210fbaf1
Saurabh <saurabhburnwal00@gmail.com>
2023-09-08 01:40:37 +0530

    add function to play a round and add user input

--- myscipt.js
+++ myscipt.js
@@ -1,6 +1,16 @@
 
+//Function to generate Computer's choice
 function getComputerChoice() { 
     const ch = ['Rock','Paper','Scissors'];
     var cpu = Math.floor(Math.random() * 3);
     return ch[cpu];
 }
+
+//Function that accepts player and computer selection and simulates one round
+function playround(playerSelection, computerSelection) { 
+    // Code Block
+}
+
+const playerSelection = prompt("Enter your choice", "Rock");
+const computerSelection = getComputerChoice();
+console.log(playround(playerSelection,computerSelection));
