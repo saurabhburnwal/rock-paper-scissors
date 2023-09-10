af387eacd823d9249eb2eb52a2a9ff8585f892c2
Saurabh <saurabhburnwal00@gmail.com>
2023-09-11 00:52:51 +0530

    write the playRound function

--- myscipt.js
+++ myscipt.js
@@ -7,10 +7,33 @@ function getComputerChoice() {
 }
 
 //Function that accepts player and computer selection and simulates one round
-function playround(playerSelection, computerSelection) { 
-    // Code Block
+function playRound(playerSelection, computerSelection) { 
+    p = playerSelection.toLowerCase();
+    if(playerSelection.equalsIgnoreCase(computerSelection)) { 
+        return "It's a Draw!";
+    } else { 
+        if(p === "rock") { 
+            if(computerSelection === "Paper") { 
+                return "You Lose! Paper beats Rock";
+            } else { 
+                return "You Win! Rock beats Scissors";
+            }
+        } else if(p === "paper") { 
+            if(computerSelection === "Scissors") { 
+                return "You Lose! Scissors beats Paper";
+            } else { 
+                return "You Win! Paper beats Rock";
+            }
+        } else { 
+            if(computerSelection === "Rock") { 
+                return "You Lose! Rock beats Scissors";
+            } else { 
+                return "You Win! Scissors beats Paper";
+            }
+        }
+    }
 }
 
 const playerSelection = prompt("Enter your choice", "Rock");
 const computerSelection = getComputerChoice();
-console.log(playround(playerSelection,computerSelection));
+console.log(playRound(playerSelection,computerSelection));
